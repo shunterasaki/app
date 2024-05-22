8e5d5d6648c1bfb629d2990315e013f1362fbdda
Shun Terasaki <shun.tera.dev@gmail.com>
2024-05-22 17:54:14 +0900

    commit

--- passwordmanager/localizable.ts
+++ passwordmanager/localizable.ts
@@ -20,13 +20,13 @@ function english(): void {
     selectedLanguage = SupportedLanguage.English;
     elementHeader.innerHTML = "Password Manager";
     elementPrivacyPolicy.innerHTML = "Privacy Policy";
-    elementPrivacyPolicy.href = "./doc/privacy/en.html";
+    elementPrivacyPolicy.href = "./doc/privacy/en";
 }
 
 function japanese(): void {
     selectedLanguage = SupportedLanguage.Japanese;
     elementHeader.innerHTML = "Password Manager - パスワード管理";
     elementPrivacyPolicy.innerHTML = "プライバシーポリシー";
-    elementPrivacyPolicy.href = "./doc/privacy/jp.html";
+    elementPrivacyPolicy.href = "./doc/privacy/jp";
 }
 
